b24d6576aea636c8665400a75d45d5100fed9447
Ale Giorgis <alegiorgis22@gmail.com>
2022-10-25 20:27:14 +0200

    Modificacion de sintaxis

--- src/main/java/com/portfoliogiorgis/Giorgis/repository/InfoRepo.java
+++ src/main/java/com/portfoliogiorgis/Giorgis/repository/InfoRepo.java
@@ -1,6 +1,6 @@
 package com.portfoliogiorgis.Giorgis.repository;
 
-import com.portfoliogiorgis.Giorgiss.models.Info;
+import com.portfoliogiorgis.Giorgis.models.Info;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InfoRepo extends JpaRepository<Info, Long> {
